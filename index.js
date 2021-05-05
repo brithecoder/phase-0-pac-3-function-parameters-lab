8f608109062e44b70d18e0f3855ef24eda8a0ce3
Brianna Gordon <brigordon574@gmail.com>
2021-05-05 11:45:07 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+function introduction(name){
+    if (name==='Josh');
+    return `Hi, my name is ${name}.` //`Hi,my name is Josh.`
+    console.log(`Hi, my name is Josh`)
+    
+}
+ function introductionWithLanguage(name,language){
+     if (name === `Josh`);
+     if (language ===`Ember.js`);
+     return`Hi, my name is ${name} and I am learning to program in ${language}.` //`Hi,my name is Josh and I am learning to program in Ember.js`
+ }
+     
+ function introductionWithLanguageOptional(name,language=`JavaScript`){
+     if (name === `Josh`);
+     if (language ===`Ember.js`);
+     return`Hi, my name is ${name} and I am learning to program in ${language}.`//`Hi,my name is Josh and I am learning to program in JavaScript.`
+     }
+
